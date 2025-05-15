361a0c0a8d81e92d5cac335402fcdd3caf8dcaba
AnGabSS <angelo.gs.santana@outlook.com>
2025-05-15 08:22:36 -0300

    [FEATURE] ADD Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { applyGlobalConfig } from './global-config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+    app.enableCors({
+    origin: ['*'], // ou '*' para todos
+    credentials: true, // se estiver usando cookies ou auth headers
+  });
+
+    const config = new DocumentBuilder()
+    .setTitle('User Manager API')
+    .setDescription(
+      'A simple user manager api with create,read,update,delete and authentication endpoints',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Infomar o JWT para autorizar o acesso',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger-ui.html', app, document)
+
   applyGlobalConfig(app)
   await app.listen(process.env.PORT ?? 3000)
 }
